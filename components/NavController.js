019347afe219452f4f01aad4987bd6fb6ef028b6
nnyam3831 <nnyam3831@naver.com>
2020-02-11 00:42:53 +0900

    Stack Navigation

--- components/NavController.js
+++ components/NavController.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useIsLoggedIn, useLogIn, useLogOut } from "../AuthContext";
 import { Text, View, TouchableOpacity } from "react-native";
+import AuthNavigation from "../navigation/AuthNavigation";
 
 export default () => {
   const isLoggedIn = useIsLoggedIn();
@@ -8,15 +9,13 @@ export default () => {
   const logOut = useLogOut();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1 }}>
       {isLoggedIn ? (
         <TouchableOpacity onPress={logOut}>
           <Text>Log Out</Text>
         </TouchableOpacity>
       ) : (
-        <TouchableOpacity onPress={logIn}>
-          <Text>Log In</Text>
-        </TouchableOpacity>
+        <AuthNavigation />
       )}
     </View>
   );
---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,17 @@
+// 인증 내비게이션
+// Auth Home / SignUp / Login / Confirm
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import AuthHome from "../screens/Auth/AuthHome";
+import LogIn from "../screens/Auth/LogIn";
+import SignUp from "../screens/Auth/SignUp";
+import Confirm from "../screens/Auth/Confirm";
+
+const AuthNavigation = createStackNavigator({
+  AuthHome,
+  SignUp,
+  LogIn,
+  Confirm
+});
+
+export default createAppContainer(AuthNavigation);
